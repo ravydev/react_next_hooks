16514c309fc63d041a523a684e4a91a9c7c297fa
Ravy Carvalho <ravyestudandoreact@gmail.com>
2023-09-19 14:38:29 -0300

    hooku useState 01

---
+++ src/components/UseState_01.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+
+export default () => {
+
+    const [valor, setValor] = useState(0);
+
+    function diminuir() {
+        setValor(v => v - 1)
+    }
+
+    function aumentar() {
+        setValor(v => v + 1)
+    }
+
+    return(
+        <>
+            <h4>useState</h4>
+            <p>Valor: {valor}</p>
+            <button onClick={diminuir}> Diminuir</button>
+            <button onClick={aumentar}>aumentar</button>
+        </>
+    )
+}
